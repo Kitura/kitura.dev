d6216700ad88288ea297189761d7fda20cf19a2e
David Dunn <davdunn2@uk.ibm.com>
2018-06-20 07:50:03 +0100

    Add home & learn pages

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "Kitura.io",
+    dependencies: [
+        .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.4.0")),
+        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.1")),
+        .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", from: "7.1.0"),
+        .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
+        .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
+        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(name: "Kitura.io", dependencies: [ .target(name: "Application"), "Kitura" , "HeliumLogger"]),
+        .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics","Health", "KituraStencil"]),
+        
+        .testTarget(name: "ApplicationTests" , dependencies: [.target(name: "Application"), "Kitura","HeliumLogger" ])
+    ]
+)
