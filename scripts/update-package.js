cd5e54fe56e1d1437736ea5efdc6614c7391dc82
David Dunn <davdunn2@uk.ibm.com>
2019-02-14 12:45:29 +0000

    enhance packages script

--- scripts/update-package.js
+++ scripts/update-package.js
@@ -1,28 +1,32 @@
 var release;
+var kituraRelease;
+getLatestKituraRelease();
 
 if (localStorage.getItem('package') !== "") {
   getLatestRelease(localStorage.getItem('package'));
 }
 
-function getLatestRelease(packageName) {
-  console.log(packageName);
+async function getLatestRelease(packageName) {
   const http = new XMLHttpRequest();
   const url = "https://api.github.com/repos/IBM-Swift/" + packageName + "/releases/latest";
   http.open("GET", url);
   http.send();
 
-  http.onreadystatechange = function() {
+  http.onreadystatechange = await async function() {
     if (this.readyState == 4 && this.status == 200) {
       var obj = JSON.parse(http.responseText);
       release = obj.name;
-      updatePackage(packageName, release);
+      await updatePackage(packageName, release);
     }
   }
 }
 
 function updatePackage(packageName, version) {
-  var package = document.getElementById('package');
-  package.innerText = (".package(url: \"https://github.com/IBM-Swift/" + packageName + ".git\", from: \"" + version + "\"),");
+  var packageValues = document.getElementsByClassName('package-value');
+  for (var x = 0; x < packageValues.length; x++) {
+    console.log(packageValues[x]);
+    packageValues[x].innerText = (".package(url: \"https://github.com/IBM-Swift/" + packageName + ".git\", from: \"" + version + "\"),");
+  }
 
   var dependencyName;
 
@@ -33,8 +37,30 @@ function updatePackage(packageName, version) {
   }
 
   var title = document.getElementById('title');
-  title.innerText = "Adding " + dependencyName + " to your Kitura App";
+  title.innerText = "Adding " + dependencyName + " To A Kitura App";
 
-  var dependency = document.getElementById('dependency');
-  dependency.innerText = ".target(name: \"MyKituraApp\", dependencies: [\"Kitura\", \"" + dependencyName + "\"]),"
+  var dependencyValues = document.getElementsByClassName('dependency-value');
+  console.log(dependencyValues);
+  for (var y = 0; y < dependencyValues.length; y++) {
+    dependencyValues[y].innerText = ".target(name: \"MyKituraApp\", dependencies: [\"Kitura\", \"" + dependencyName + "\"]),"
+  }
+}
+
+function getLatestKituraRelease() {
+  const http = new XMLHttpRequest();
+  const url = "https://api.github.com/repos/IBM-Swift/Kitura/releases/latest";
+  http.open("GET", url);
+  http.send();
+  http.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var obj = JSON.parse(http.responseText);
+      kituraRelease = obj.name;
+      updateToLatestKituraRelease();
+    }
+  }
+}
+
+function updateToLatestKituraRelease() {
+  var element = document.getElementById('kitura');
+  element.innerText = (".package(url: \"https://github.com/IBM-Swift/Kitura.git\", from: \"" + kituraRelease + "\"),");
 }
