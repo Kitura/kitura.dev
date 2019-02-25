fcd04d014cfd1ea671ff0a2f916c5bcd8f9d0cd5
David Dunn <davdunn2@uk.ibm.com>
2019-02-25 15:51:05 +0000

    Add initial tabbing in Kuery guide

--- scripts/docs.js
+++ scripts/docs.js
@@ -49,3 +49,17 @@ function removePopup() {
   var popup = document.getElementById('popup');
   popup.style.display = "none";
 }
+
+function openPlugin(evt, pluginName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(pluginName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
