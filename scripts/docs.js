4b0e3d309cdd63ec09e03c1a44b0f3c593446fa5
David Dunn <dd4dev@gmail.com>
2019-04-15 15:17:16 +0100

    Update the learn page to ensure users can bookmark the docs (#292)
    * Initial refactor
    
    * Add prism for syntax highlighting
    
    * Add create server pages
    
    * Update favicon
    
    * Update page titles
    
    * Add active classes
    
    * Fix broken links
    
    * Fix mobile usage
    
    * Enable back to top script
    
    * Fix broken packages links
    
    * Fix link to ssl guide
    
    * Fix toggles in guides
    
    * Add max-width to docs window
    
    * Fix typo in css class name
    
    * Update font size

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,20 +1,5 @@
 addCopyFunction();
 
-function removeActiveSidebarElementForParent() {
-  var parentSelectables = parent.document.getElementsByClassName('selectable');
-  for (var j = 0; j < parentSelectables.length; j++) {
-    if (parentSelectables[j].classList.contains('active')) {
-      parentSelectables[j].classList.remove('active');
-    }
-  }
-}
-
-function setActiveSidebarElementForParent(id) {
-  removeActiveSidebarElementForParent()
-  localStorage.setItem("id", id);
-  parent.document.getElementById(id).classList.add('active');
-}
-
 function addCopyFunction() {
   var codeSamples = document.getElementsByTagName("pre");
   for (var x = 0; x < codeSamples.length; x++) {
