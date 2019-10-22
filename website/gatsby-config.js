ba791e2b823a51153517bbcfa68acc74cad389d1
ddunn2 <dd4dev@gmail.com>
2019-10-22 14:11:36 +0100

    Update image name in gatsby-config

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: `src/images/kitura.svg`
+        icon: `src/images/Kitura.svg`
       },
     },
     {
